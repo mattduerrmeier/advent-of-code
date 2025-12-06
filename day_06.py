0823747f3faff1aca42dfdb6ab2779426bbbba1b
Mattias Dürrmeier <matt.duerr@pm.me>
2025-12-06 21:46:11 +0100

    Add day 6 part 1

---
+++ day_06.py
@@ -0,0 +1,38 @@
+import math
+
+def solve_1(lines: list[str]) -> int:
+    answers: list[int] = []
+    acc: list[str] = [""] * len(lines)
+
+    for i in range(len(lines[0])):
+        chars = [""] * len(lines)
+        for j in range(len(lines)):
+            chars[j] = lines[j][i]
+            if chars[j] != " ":
+                acc[j] += chars[j]
+
+        if all([c == " " for c in chars]) or i == len(lines[0])-1:
+            values = [int(val) for val in acc if val not in ["*", "+"]]
+            operator = acc[-1]
+            if operator == "*":
+                result = math.prod(values)
+            elif operator == "+":
+                result = sum(values)
+
+            answers.append(result)
+            acc = [""] * len(lines)
+
+    return(sum(answers))
+
+
+with open("input06.txt", "r") as f:
+    lines = f.read().rstrip("\n").split("\n")
+
+example = [
+    "123 328  51 64 ",
+    " 45 64  387 23 ",
+    "  6 98  215 314",
+    "*   +   *   +  ",
+]
+
+print("Answer 1:", solve_1(lines))
