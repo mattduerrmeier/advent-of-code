91a7fac8f89193b2e4ba4e1b778a3a1b521238ae
Mattias Dürrmeier <matt.duerr@pm.me>
2025-12-06 22:20:44 +0100

    Add day 6 part 2

--- day_06.py
+++ day_06.py
@@ -22,7 +22,43 @@ def solve_1(lines: list[str]) -> int:
             answers.append(result)
             acc = [""] * len(lines)
 
-    return(sum(answers))
+    return sum(answers)
+
+def solve_2(lines: list[str]) -> int:
+    answers: list[int] = []
+    acc: list[str] = [""] * len(lines)
+
+    for i in range(len(lines[0])):
+        chars = [""] * len(lines)
+        for j in range(len(lines)):
+            chars[j] = lines[j][i]
+            acc[j] += chars[j]
+
+        if all([c == " " for c in chars]) or i == len(lines[0])-1:
+            operator = acc[-1].rstrip()
+            values = acc[:-1]
+
+            sub = 1 if (i != len(lines[0])-1) else 0
+            num_values = max([len(c.rstrip()) for c in values])
+            re_order: list[int] = [0] * num_values
+            for x in range(num_values):
+                val = ""
+                for y in range(len(values)):
+                    val += values[y][x]
+
+                re_order[x] = int(val)
+
+            result: int = 0
+            if operator == "*":
+                result = math.prod(re_order)
+            elif operator == "+":
+                result = sum(re_order)
+
+            answers.append(result)
+            acc = [""] * len(lines)
+
+    return sum(answers)
+
 
 
 with open("input06.txt", "r") as f:
@@ -36,3 +72,4 @@ example = [
 ]
 
 print("Answer 1:", solve_1(lines))
+print("Answer 2:", solve_2(lines))
