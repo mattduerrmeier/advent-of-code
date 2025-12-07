3ca433120aa912f99ed2f293e3df3fd63d964bc6
Mattias Dürrmeier <matt.duerr@pm.me>
2025-12-07 14:09:18 +0100

    Add day 7 part 2

--- day_07.py
+++ day_07.py
@@ -18,10 +18,40 @@ def solve_1(lines: list[list[str]]) -> int:
 
     return counter
 
+def solve_2(lines: list[list[str]]) -> int:
+    split_idxs = set()
+    split_idxs.add(lines[0].index("S"))
+
+    for j in range(1, len(lines)):
+        temp_split = set()
+        for i in range(len(lines[0])):
+            if lines[j][i] == "^" and lines[j-1][i].isdigit():
+                if lines[j][i-1].isdigit():
+                    lines[j][i-1] = str(int(lines[j][i-1]) + int(lines[j-1][i]))
+                else:
+                    lines[j][i-1] = lines[j-1][i]
+
+                lines[j][i+1] = lines[j-1][i]
+
+                split_idxs.remove(i)
+                temp_split.add(i-1)
+                temp_split.add(i+1)
+            elif i in split_idxs:
+                if lines[j-1][i].isdigit():
+                    if lines[j][i].isdigit():
+                        lines[j][i] = str(int(lines[j][i]) + int(lines[j-1][i]))
+                    else:
+                        lines[j][i] = lines[j-1][i]
+                else:
+                    lines[j][i] = str(1)
+
+        split_idxs = split_idxs.union(temp_split)
+
+    return(sum([int(i) for i in lines[-1] if i != "."]))
+
 
 with open("input07.txt", "r") as f:
     lines = f.read().rstrip("\n").split("\n")
-
 
 example = [
     ".......S.......",
@@ -46,3 +76,4 @@ example = [list(i) for i in example]
 lines = [list(i) for i in lines]
 
 print("Answer 1:", solve_1(lines))
+print("Answer 2:", solve_2(lines))
